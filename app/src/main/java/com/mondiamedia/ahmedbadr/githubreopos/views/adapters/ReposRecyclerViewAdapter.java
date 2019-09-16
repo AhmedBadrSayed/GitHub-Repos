0b824c40952c6a19352e08165b21a0b1fce9a738
AHBA1 <ahmed.badr@mondiamedia>
2019-09-16 23:15:16 +0200

    implemented expanding and collapsing in repos list items

--- app/src/main/java/com/mondiamedia/ahmedbadr/githubreopos/views/adapters/ReposRecyclerViewAdapter.java
+++ app/src/main/java/com/mondiamedia/ahmedbadr/githubreopos/views/adapters/ReposRecyclerViewAdapter.java
@@ -13,17 +13,22 @@ import com.mondiamedia.ahmedbadr.githubreopos.R;
 import com.mondiamedia.ahmedbadr.githubreopos.models.GitHubRepository;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ReposRecyclerViewAdapter extends RecyclerView.Adapter<ReposRecyclerViewAdapter.ReposViewHolder> {
 
-
     private List<GitHubRepository> mGitHubRepositoryList;
+    private List<Boolean> mCollapsingStatesList;
     private Context mContext;
 
     public ReposRecyclerViewAdapter(List<GitHubRepository> repositories, Context context) {
-        this.mGitHubRepositoryList = repositories;
         this.mContext = context;
+        this.mGitHubRepositoryList = repositories;
+        this.mCollapsingStatesList = new ArrayList<>(mGitHubRepositoryList.size());
     }
 
     @Override
@@ -37,6 +42,11 @@ public class ReposRecyclerViewAdapter extends RecyclerView.Adapter<ReposRecycler
     public void onBindViewHolder(ReposViewHolder holder, int position) {
         GitHubRepository gitHubRepository = mGitHubRepositoryList.get(position);
 
+        mCollapsingStatesList.clear();
+        while (mCollapsingStatesList.size() < mGitHubRepositoryList.size()) {
+            mCollapsingStatesList.add(false);
+        }
+
         Picasso.with(mContext).load(gitHubRepository.getAvatar()).into(holder.authorImage);
         holder.authorImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
         holder.authorName.setText(gitHubRepository.getAuthor());
@@ -45,6 +55,21 @@ public class ReposRecyclerViewAdapter extends RecyclerView.Adapter<ReposRecycler
         holder.language.setText(gitHubRepository.getLanguage());
         holder.starsCount.setText(gitHubRepository.getStars());
         holder.forksCount.setText(gitHubRepository.getForks());
+
+        holder.cardView.setOnClickListener(v -> {
+            updateViews(holder, !mCollapsingStatesList.get(position));
+            mCollapsingStatesList.set(position, !mCollapsingStatesList.get(position));
+        });
+    }
+
+    private void updateViews(ReposViewHolder holder, boolean show) {
+        holder.repoDescription.setVisibility(show ? View.VISIBLE : View.GONE);
+        holder.languageIcon.setVisibility(show ? View.VISIBLE : View.GONE);
+        holder.language.setVisibility(show ? View.VISIBLE : View.GONE);
+        holder.starIcon.setVisibility(show ? View.VISIBLE : View.GONE);
+        holder.starsCount.setVisibility(show ? View.VISIBLE : View.GONE);
+        holder.forkIcon.setVisibility(show ? View.VISIBLE : View.GONE);
+        holder.forksCount.setVisibility(show ? View.VISIBLE : View.GONE);
     }
 
     @Override
@@ -59,25 +84,32 @@ public class ReposRecyclerViewAdapter extends RecyclerView.Adapter<ReposRecycler
 
     public class ReposViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        @BindView(R.id.repo_card)
         CardView cardView;
+        @BindView(R.id.author_image)
         ImageView authorImage;
+        @BindView(R.id.author_name)
         TextView authorName;
+        @BindView(R.id.repo_title)
         TextView repoTitle;
+        @BindView(R.id.repo_description)
         TextView repoDescription;
+        @BindView(R.id.language_icon)
+        ImageView languageIcon;
+        @BindView(R.id.language)
         TextView language;
+        @BindView(R.id.star_icon)
+        ImageView starIcon;
+        @BindView(R.id.stars_count)
         TextView starsCount;
+        @BindView(R.id.fork_icon)
+        ImageView forkIcon;
+        @BindView(R.id.forks_count)
         TextView forksCount;
 
         public ReposViewHolder(View itemView) {
             super(itemView);
-            cardView = itemView.findViewById(R.id.repo_card);
-            authorImage = itemView.findViewById(R.id.author_image);
-            authorName = itemView.findViewById(R.id.author_name);
-            repoTitle = itemView.findViewById(R.id.repo_title);
-            repoDescription = itemView.findViewById(R.id.repo_description);
-            language = itemView.findViewById(R.id.language);
-            starsCount = itemView.findViewById(R.id.stars_count);
-            forksCount = itemView.findViewById(R.id.forks_count);
+            ButterKnife.bind(this, itemView);
             itemView.setOnClickListener(this);
         }
 
